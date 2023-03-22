be10bccc2aa57feda0477f7865e9ebcd4d09f43d
Isaac Basque-Rice <ibasquerice@gmail.com>
2023-03-22 15:08:19 +0000

    Pep8 compliance work, added docstrings

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
-import os
-from discord.ext import commands
-import asyncio
 import json
 import random
 import re
+
+from discord.ext import commands
+
 from markovboi import MarkovBoi
 
 m = MarkovBoi()
@@ -13,12 +13,39 @@ with open('secrets.json', 'r') as f:
 
 bot = commands.Bot(command_prefix='!')
 
+
 @bot.event
 async def on_ready():
+    """
+    Function to handle the 'on_ready' event for a Discord bot.
+
+    This function is triggered when the bot is ready to receive and respond to commands. It prints a message to the
+        console indicating that the bot is online and ready to use.
+
+    Returns:
+        None
+    """
+
     print(f'{bot.user} is in the house!!')
+
 
 @bot.event
 async def on_message(message):
+    """
+    Function to handle the 'on_message' event for a Discord bot.
+
+    This function is triggered whenever a message is sent in a Discord channel that the bot has access to. It performs
+    several tasks on the message, including printing its content to the console, parsing it with a 'm' object,
+    processing any commands included in the message, and occasionally sending a generated message to the channel.
+
+    Args:
+        message (discord.Message): The message object containing the message content, author information, and other
+        metadata.
+
+    Returns:
+        None
+    """
+
     if message.author.bot:
         return
 
@@ -28,28 +55,64 @@ async def on_message(message):
 
     await bot.process_commands(message)
 
-    if random.randint(1,100) < 5:
+    if random.randint(1, 100) < 5:
         await message.channel.send(m.gen_message('000000000000000000'))
 
+
 @bot.command()
-async def scan(ctx, user=None):
+async def scan(ctx):
+    """
+    Function to handle the 'scan' command for a Discord bot.
+
+    This function is triggered whenever the 'scan' command is used in a Discord channel. It scans the last 10,000
+    messages in the channel and indexes each non-bot message using a 'm' object. It then sends a message to the
+    channel indicating how many messages were scanned and indexed.
+
+    Args:
+        ctx (discord.ext.commands.Context): The context object representing the context in which the command was used.
+        Contains information about the message, author, and other metadata.
+
+    Returns:
+        None
+    """
     count = 0
 
     async for msg in ctx.message.channel.history(limit=10000):
-        if not msg.author.bot and not msg.content.startswith(('-','s?', '!')):
+        if not msg.author.bot and not msg.content.startswith(('-', 's?', '!')):
             count += 1
             m.parse_message(str(msg.author.id), msg.content)
 
     await ctx.channel.send(f'last {count} messages in this channel scanned and indexed')
 
+
 @bot.command()
 async def copy(ctx, user=None, seed=None):
+    """
+    Function to handle the 'copy' command for a Discord bot.
+
+    This function is triggered whenever the 'copy' command is used in a Discord channel. It takes an optional 'user'
+    argument, which specifies which user's message history to copy, and an optional 'seed' argument, which is used
+    to generate a specific message. If no user is specified, the function copies the message history of the user who
+    invoked the command.
+
+    Args:
+        ctx (discord.ext.commands.Context): The context object representing the context in which the command was used.
+        Contains information about the message, author, and other metadata.
+
+        user (str, optional): A string representing the user ID of the user whose message history should be copied.
+        Defaults to None.
+
+        seed (str, optional): A string representing the seed used to generate a specific message. Defaults to None.
+
+    Returns:
+        None
+    """
     if user and user.lower() == 'all':
         user = '000000000000000000'
     elif user:
         try:
             user = re.findall(r'\d{18}', user)[0]
-        except:
+        except IndexError:
             await ctx.channel.send('**error:** first arg must be a valid user or "all"')
     else:
         user = str(ctx.author.id)
@@ -61,5 +124,6 @@ async def copy(ctx, user=None, seed=None):
         await ctx.channel.send(m.gen_message(user, seed))
     else:
         await ctx.channel.send(m.gen_message(user))
+
 
 bot.run(TOKEN)
--- markovboi.py
+++ markovboi.py
@@ -1,8 +1,54 @@
-import sys
 import random
 import redis
 
-class MarkovBoi():
+
+def make_key(user, k):
+    """
+    Concatenates the user and key with a hyphen in between and returns the resulting bytes object.
+
+        Args:
+            user: The user identifier as bytes.
+            k: The key as bytes.
+
+        Returns:
+            A bytes object that is the concatenation of user and key with a hyphen in between.
+    """
+    return b'-'.join((user, k))
+
+
+def parse_key(user, k):
+    """
+    Extracts the key from a bytes object that contains the user and key concatenated with a hyphen in between.
+
+        Args:
+            user: The user identifier as bytes.
+            k: The bytes object that contains the user and key concatenated with a hyphen in between.
+
+        Returns:
+            A bytes object that is the key extracted from the input bytes object.
+    """
+    return k[len(user) + 1:]
+
+
+class MarkovBoi:
+    """
+       MarkovBoi class generates messages using Markov Chain algorithm. It generates random messages using
+       redis database with a chain length of 2.
+
+       Attributes:
+           chain_length (int): Length of the chain.
+           end_word (str): A special character to mark the end of the message.
+           separator (str): A special character used to separate the words in the keys.
+           prefix (bytes): The prefix used to create keys in the redis database.
+           avg_length (int): The average length of the message to generate.
+           all_user (str): A special user identifier used for collecting all the messages.
+
+       Methods:
+           __init__(): Initializes the redis client.
+           split_message(message): Splits a message into a list of lists of words.
+           gen_message(user, seed=None): Generates a message using redis database.
+           parse_message(user, message): Parses a message and stores it in the redis database.
+   """
 
     chain_length = 2
     end_word = '\x02'
@@ -12,16 +58,20 @@ class MarkovBoi():
     all_user = '000000000000000000'
 
     def __init__(self):
+        """
+        Initializes the redis client.
+        """
         super(MarkovBoi, self).__init__()
         self.r = redis.Redis()
 
-    def make_key(self, user, k):
-        return b'-'.join((user, k))
-
-    def parse_key(self, user, k):
-        return k[len(user)+1:]
-
     def split_message(self, message):
+        """
+        Splits a message into a list of lists of words.
+            Args:
+                message (str): The message to split.
+            Returns:
+                A generator object that yields lists of words.
+        """
         words = message.split()
 
         if len(words) > self.chain_length:
@@ -30,49 +80,67 @@ class MarkovBoi():
                 yield words[i:i + self.chain_length + 1]
 
     def gen_message(self, user, seed=None):
+        """
+        Generates a message using redis database.
+
+            Args:
+                user (str): The user for which the message is being generated.
+                seed (str): The seed for the message. If None, a random seed is chosen from the redis database.
+
+            Returns:
+                A string representing the generated message.
+        """
         if seed:
             try:
-                key = self.parse_key(
-                    user, 
+                key = parse_key(
+                    user,
                     random.sample(list(self.r.scan_iter(match=f'*0000-{seed}*')), 1)[0]
                 )
             except:
                 return '**error:** no messages for that seed :(('
         else:
-            key = self.parse_key(user, self.r.srandmember(user + '-keys'))
+            key = parse_key(user, self.r.srandmember(user + '-keys'))
 
         message = []
-        
+
         for i in range(self.avg_length):
             # get the words from the key
             words = key.split(self.separator.encode())
-            
+
             # add the word to final message
             message.append(words[0])
 
             # get next word based on last key
-            next_word = self.r.srandmember(self.make_key(user.encode(), key))
-            
+            next_word = self.r.srandmember(make_key(user.encode(), key))
+
             # these words are fresh :O
             if not next_word:
-           	    break
-            
+                break
+
             # create new key
             key = words[-1] + self.separator.encode() + next_word
 
         return b' '.join(message).decode()
-        
+
     def parse_message(self, user, message):
+        """
+        Parses a message and stores it in the redis database.
+
+            Args:
+                user (str): The user who sent the message.
+                message (str): The message to parse and store in the redis database.
+        """
         for words in self.split_message(message.lower()):
             key = self.separator.join(words[:-1])
-            
+
             # add the message keys
-            self.r.sadd(self.make_key(user.encode(), key.encode()), words[-1])
-            self.r.sadd(self.make_key(self.all_user.encode(), key.encode()), words[-1])
+            self.r.sadd(make_key(user.encode(), key.encode()), words[-1])
+            self.r.sadd(make_key(self.all_user.encode(), key.encode()), words[-1])
 
             # add the keys to the collections of keys
-            self.r.sadd(user + '-keys', self.make_key(user.encode(), key.encode()))
-            self.r.sadd(self.all_user + '-keys', self.make_key(self.all_user.encode(), key.encode()))
+            self.r.sadd(user + '-keys', make_key(user.encode(), key.encode()))
+            self.r.sadd(self.all_user + '-keys', make_key(self.all_user.encode(), key.encode()))
+
 
 if __name__ == '__main__':
     m = MarkovBoi()
